8bd2ca1720decd385816a7cf76be17ee2b2c215b
nastu <nastu7@gmail.com>
2017-01-16 17:27:38 +0100

    Changes

---
+++ public/templates/partials/singleNodeListed.php
@@ -0,0 +1,7 @@
+<div class="col-lg-12">
+    <!-- small box -->
+    <div class="callout callout-{CALLOUT} text-center">
+        <span class="">{BSCNR}-{TGNR}-{NAME}-{CONFNR}</span>
+    </div>
+
+</div>
